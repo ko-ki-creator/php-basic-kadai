bb66b9b99306d35da4e7267eff3bf69baf886db2
ko-ki-creator <c9xj.c9ajmf2@gmail.com>
2024-08-21 17:01:47 +0900

    テストの平均点を計算しよう

---
+++ kadai_005/score.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+  <meta charset="UTF-8">
+  <title>テストの平均点を計算</title>
+</head>
+
+<body>
+  <p>
+    <?php
+    // スコア変数に値を代入する
+    $score1 = 80;
+    $score2 = 60;
+    $score3 = 55;
+    $score4 = 40;
+    $score5 = 100;
+    $score6 = 25;
+    $score7 = 80;
+    $score8 = 95;
+    $score9 = 30;
+    $score10 = 60;
+
+    // 合計スコア
+    $score_total = $score1 + $score2 + $score3 + $score4 + $score5 + $score6 + $score7 + $score8 + $score9 + $score10;
+
+    // 平均点
+    $score_average = $score_total / 10;
+    echo $score_average;
+    ?>
+  </p>
+</body>
+</html>
