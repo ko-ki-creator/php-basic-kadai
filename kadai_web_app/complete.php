8d75a2dd32db3b0f4e67a69aa2b570a35b1bbf24
ko-ki-creator <c9xj.c9ajmf2@gmail.com>
2024-08-26 14:12:49 +0900

    社員情報入力フォームを作成しよう

---
+++ kadai_web_app/complete.php
@@ -0,0 +1,20 @@
+<?php
+// セッションを開始
+session_start();
+
+?>
+
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+  <meta charset="UTF-8">
+  <title>PHP課題</title>
+</head>
+
+<body>
+  <h2>登録が完了しました</h2>
+  <p>社員情報がデータベースに保存されました。</p>
+  <button id="back-btn" onclick="location.href='form.php';">戻る</button>
+</body>
+</html>
